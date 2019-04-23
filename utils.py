f736cac7a34eb3e6e70ad2da2e54ec77dbfc20f7
patrickcty <chengtiyanyang@gmail.com>
2019-04-23 16:47:58 +0800

    Update some manga image selectors.

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from tensorflow.python.client import device_lib
 from tqdm import tqdm
 
-from config import alpha, identity_annot_filename, num_train_samples, lfw_folder
+from config import alpha, identity_annot_filename, num_train_samples, lfw_folder, manga_dir, backbone_type
 
 
 def ensure_folder(folder):
@@ -70,6 +70,34 @@ def get_data_stats():
     return list(ids), sorted(images), image2id, id2images
 
 
+def get_manga_data_stats(usage='train'):
+
+    ids = set()
+    images = []
+    image2id = {}
+    id2images = {}
+
+    for single_manga_dir in os.listdir(os.path.join(manga_dir, usage)):
+        if single_manga_dir.startswith('.'):
+            continue
+
+        for manga_face in os.listdir(os.path.join(manga_dir, usage, single_manga_dir)):
+            if manga_face.startswith('.'):
+                continue
+
+            image_path = os.path.join(single_manga_dir, manga_face)
+            image_id = manga_face.split('_')[0]
+            ids.add(image_id)
+            images.append(image_path)
+            image2id[image_path] = image_id
+            if image_id in id2images.keys():
+                id2images[image_id].append(image_path)
+            else:
+                id2images[image_id] = [image_path]
+
+    return list(ids), sorted(images), image2id, id2images
+
+
 def triplet_loss(y_true, y_pred):
     a_pred = y_pred[:, 0:128]
     p_pred = y_pred[:, 128:256]
@@ -112,8 +140,45 @@ def get_random_triplets():
     return data_set
 
 
+def get_random_manga_triplets(usage='train'):
+    # Random selection of validation set samples
+    ids, images, image2id, id2images = get_manga_data_stats(usage)
+
+    data_set = []
+
+    for i in tqdm(range(len(images))):
+        # choose a_image
+        while True:
+            a_image = random.choice(images)
+            a_id = image2id[a_image]
+            if len(id2images[a_id]) >= 2:
+                break
+
+        # choose p_image
+        while True:
+            p_image = random.choice(id2images[a_id])
+            if p_image != a_image:
+                break
+
+        # choose n_image
+        while True:
+            n_image = random.choice(images)
+            n_id = image2id[n_image]
+            if n_id != a_id:
+                break
+
+        data_set.append({'a': a_image, 'p': p_image, 'n': n_image})
+
+    return data_set
+
+
 def get_train_images():
     _, images, _, _ = get_data_stats()
+    return images
+
+
+def get_manga_test_images():
+    _, images, _, _ = get_manga_data_stats('test')
     return images
 
 
@@ -168,11 +233,52 @@ def get_lfw_pairs():
     return pairs
 
 
-def get_smallest_loss():
+def get_random_manga_pairs():
+
+    ids, images, image2id, id2images = get_manga_data_stats('test')
+    pairs = []
+
+    min_face_num = 100000
+    for key, values in id2images.items():
+        if len(values) < min_face_num:
+            min_face_num = len(values)
+
+    for i in (range(len(ids))):
+        label_img_list = id2images[ids[i]][:min_face_num]
+        for j in range(len(label_img_list)):
+            for k in range(1, len(label_img_list)):
+                pairs.append({'image_name_1': label_img_list[j],
+                              'image_name_2': label_img_list[k], 'same_person': True})
+        for img in label_img_list:
+            while True:
+                random_img = random.choice(images)
+                if image2id[random_img] != image2id[img]:
+                    break
+            pairs.append({'image_name_1': img,
+                          'image_name_2': random_img, 'same_person': False})
+
+    return pairs
+
+    # manga_triplets = get_random_manga_triplets('test')
+    # pairs = []
+    #
+    # for i in (range(len(manga_triplets))):
+    #     a_image = manga_triplets[i]['a']
+    #     p_image = manga_triplets[i]['p']
+    #     n_image = manga_triplets[i]['n']
+    #     pairs.append({'image_name_1': a_image, 'image_name_2': p_image, 'same_person': True})
+    #     pairs.append({'image_name_1': a_image, 'image_name_2': n_image, 'same_person': False})
+    #
+    # return pairs
+
+
+def get_smallest_loss(backbone=None):
     import re
     pattern = 'model.(?P<epoch>\d+)-(?P<val_loss>[0-9]*\.?[0-9]*).hdf5'
     p = re.compile(pattern)
-    losses = [float(p.match(f).groups()[1]) for f in os.listdir('models/') if p.match(f)]
+    if not backbone:
+        backbone = backbone_type
+    losses = [float(p.match(f).groups()[1]) for f in os.listdir(backbone + '/models/') if p.match(f)]
     if len(losses) == 0:
         import sys
         return sys.float_info.max
@@ -180,10 +286,12 @@ def get_smallest_loss():
         return np.min(losses)
 
 
-def get_latest_model():
+def get_latest_model(backbone=None):
     import glob
     import os
-    files = glob.glob('models/*.hdf5')
+    if not backbone:
+        backbone = backbone_type
+    files = glob.glob(backbone + '/models/*.hdf5')
     files.sort(key=os.path.getmtime)
     if len(files) > 0:
         return files[-1]
@@ -191,15 +299,19 @@ def get_latest_model():
         return None
 
 
-def get_best_model():
+def get_best_model(backbone=None):
     import re
     pattern = 'model.(?P<epoch>\d+)-(?P<val_loss>[0-9]*\.?[0-9]*).hdf5'
     p = re.compile(pattern)
-    files = [f for f in os.listdir('models/') if p.match(f)]
+    if not backbone:
+        backbone = backbone_type
+    if not os.path.exists(backbone + '/models/'):
+        os.mkdir(backbone + '/models/')
+    files = [f for f in os.listdir(backbone + '/models/') if p.match(f)]
     filename = None
     if len(files) > 0:
         losses = [float(p.match(f).groups()[1]) for f in files]
         best_index = int(np.argmin(losses))
-        filename = os.path.join('models', files[best_index])
+        filename = os.path.join(backbone, 'models', files[best_index])
         print('loading best model: {}'.format(filename))
     return filename
